f7d81a0db1c4dc54f3eb1f52f6d65b2b439accb8
Jeevendra Singh <jeesingh@microsoft.com>
2023-05-19 22:04:15 +0530

    get user by Id

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -14,8 +14,20 @@ exports.findAll = async (req, res) => {
     }
 }
 
-exports.findById = (req, res) => {
+exports.findById = async (req, res) => {
+    const userIdRequest = req.params.userId
 
+    const user = await User.find({
+        userId: userIdRequest
+    })
+
+    if(user.length > 0){
+        return res.status(200).send(convertUserObject.userResponse(user))
+    }else{
+        return res.status(200).send({
+            message: `User with id ${userIdRequest} is not present`
+        })
+    }
 }
 
 exports.update = (req, res) => {
