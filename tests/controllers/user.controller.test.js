0f8ef5f9f546da979e3024bf56252cbeda5ee767
Jeevendra Singh <jeesingh@microsoft.com>
2023-06-02 20:49:58 +0530

    update unit test for user controller

--- tests/controllers/user.controller.test.js
+++ tests/controllers/user.controller.test.js
@@ -1,10 +1,51 @@
-describe("FindAll", () => {
-    it("Should pass and return users", () => {
+const {mockRequest, mockResponse} = require('../interceptor')
+const User = require('../../models/user.model')
+const {update, findAll, findById} = require('../../controllers/user.controller')
 
+const userTestPayload = {
+    userId: 1,
+    name: "Test",
+    password: "1234567",
+    userType: "CUSTOMER",
+    userStatus: "APPROVED",
+    email: "test@gmail.com",
+    ticketsCreated: [],
+    ticketsAssigned: []
+}
+
+describe("FindAll", () => {
+    it("Should pass and return users", async () => {
+        const userSpy = jest.spyOn(User, 'find').mockReturnValue(Promise.resolve([userTestPayload]))
+        const req = mockRequest()
+        const res = mockResponse()
+
+        await findAll(req, res)
+
+        expect(userSpy).toHaveBeenCalled()
+        expect(res.status).toHaveBeenCalledWith(200)
+        expect(res.send).toHaveBeenCalledWith(
+            expect.arrayContaining([
+                expect.objectContaining({
+                    email: "test@gmail.com",
+                    name: "Test",
+                    userId: 1,
+                    userStatus: "APPROVED",
+                    userType: "CUSTOMER"
+                })
+            ])
+        )
     })
 
-    it("Should fail", () => {
+    it("Should fail", async () => {
+        const userSpy = jest.spyOn(User, 'find').mockImplementation(() => {throw new Error("Error")})
+        const req = mockRequest()
+        const res = mockResponse()
 
+        await findAll(req, res)
+
+        expect(userSpy).toHaveBeenCalled()
+        expect(res.status).toHaveBeenCalledWith(500)
+        
     })
 })
 
